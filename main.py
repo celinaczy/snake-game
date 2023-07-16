e9f22033a59e25d6a870dce155d4dec1b258419c
celinaczy <celina.czyszczon@gmail.com>
2023-07-15 21:02:51 -0400

    update snake files

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from turtle import Screen
 from snake import Snake
 from food import Food
 import time
+from scoreboard import Scoreboard
+
 
 
 screen = Screen()
@@ -13,6 +15,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -30,6 +33,7 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 
 
